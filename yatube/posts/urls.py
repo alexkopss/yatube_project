cedbd43762d8066a5d4adf81ab770c08a9cd44f8
Александр Копосов <alexkopss@yandex.ru>
2022-03-01 16:31:29 +0300

    Добавлены urls и view-функции, slug doesnt work

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -17,5 +17,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('group/<slug:slug>/', views.group_posts)
 ] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Главная страница
+def index(request):    
+    return HttpResponse('Главная страница')
+
+
+# Страница со списком мороженого
+def group_posts(request):
+    return HttpResponse('Список групповых постов')
