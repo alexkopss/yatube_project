327439f86103edd5ea8cdbb4704f08183e412ff4
Александр Копосов <alexkopss@yandex.ru>
2022-03-08 08:58:25 +0300

    commit for checking. Doesnt work well

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,5 @@
+from tokenize import group
+from turtle import title
 from django.db import models
 from django.contrib.auth import get_user_model
 
@@ -11,4 +13,23 @@ class Post(models.Model):
         User,
         on_delete=models.CASCADE,
         related_name='posts'
-    ) 
+        ) 
+    group = models.ForeignKey(
+        'Group',
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='posts'
+        )
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(
+        unique=True
+        )
+    description = models.TextField()
+
+
+    def __str__(self) -> str:
+        return self.title
