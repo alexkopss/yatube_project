cb3d241239e8655d448201fda49574ae971fde35
Александр Копосов <alexkopss@yandex.ru>
2022-03-10 21:41:52 +0300

    project is ok

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 # Главная страница
@@ -16,10 +16,19 @@ def index(request):
 
 
 # Страница со списком мороженого
-def group_posts(request):
-    template = 'posts/group_list.html'
-    text = 'Здесь будет информация о группах проекта Yatube'
+def group_posts(request, slug):
+    # Функция get_object_or_404 получает по заданным критериям объект 
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'text': text,
+        'group': group,
+        'posts': posts,
     }
-    return render(request, template, context) 
+    return render(request, 'posts/group_list.html', context) 
